59f7196d70fa6774328e16aa2c2108809027b193
Swaleha begum Mulla <mullaswaleha402@gmail.com>
2024-03-13 11:50:55 +0530

    js_day_1

---
+++ js_day_1/app.js
@@ -0,0 +1,43 @@
+// basic
+
+// console.log("Hi js world!");
+// prompt("enter your name!");
+// alert("error!");
+
+//Variables, objects and datatypes
+
+console.log ("tutorial on variables,objects and dataTypes");
+
+// var a=5;
+// var b=6;
+//  console.log(a+b);
+
+//  var a=5;
+// var b=6;
+// var c= "swaleha"
+//  console.log(a+b+c);
+
+//  var a=5;
+// var b=6;
+
+//  console.log(a+b+2);
+
+//  var x=5;
+//  var y=null;
+// var z= "swaleha"
+// const p=true;
+// let q= undefined;
+// let r=null;
+
+// console.log(x,y,z,p,q,r);
+// console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+
+// javascript conditional : if,else,else if, else ladder
+
+let age = 14;
+let grace = 2;
+
+console.log (age+grace);
+
+
